8cd64505556f3a68db7d953c97529b2d4fae74a6
Di0gen2S <csharp_learning@outlook.com>
2026-02-15 14:53:02 +0100

    feat: initialisation projet C# avec Raylib et test fenêtre

---
+++ MonJeuRTS/Program.cs
@@ -0,0 +1,26 @@
+﻿using Raylib_cs;
+
+namespace MonJeuRTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialisation de la fenêtre
+            Raylib.InitWindow(800, 600, "Test Raylib - Mon RTS");
+            Raylib.SetTargetFPS(60);
+
+            // Boucle principale
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.DarkGreen);
+                Raylib.DrawText("Raylib fonctionne !", 250, 280, 30, Color.White);
+                Raylib.EndDrawing();
+            }
+
+            // Fermeture
+            Raylib.CloseWindow();
+        }
+    }
+}
